1d8a879c2fbd0317b0725fa872eb988f73e6b540
jprekz <jprekz@users.noreply.github.com>
2014-09-10 02:22:43 +0900

    Add PuzzleSolving project and define IPuzzleSolving interface

---
+++ PuzzleSolving/IPuzzleSolving.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolving
+{
+    interface IPuzzleSolving
+    {
+        void Start();
+
+        void Stop();
+
+        String GetAnswerString();
+
+        int GetAnswerCost();
+
+        event EventHandler FindBestAnswer;
+
+        event EventHandler FindBetterAnswer;
+
+        event EventHandler SolvingError;
+    }
+}
