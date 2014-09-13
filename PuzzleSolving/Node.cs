874b0b59e0b3028833fd822e29f7601af64341bd
jprekz <jprekz@users.noreply.github.com>
2014-09-13 17:22:30 +0900

    Add class definitions to PuzzleSolving

---
+++ PuzzleSolving/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolving
+{
+    class Node
+    {
+        public byte[,] Cells;
+        public byte Selecting;
+        public byte SelectNum;
+        public int Heuristic;
+        public Node From;
+        public Edge Swaped;
+        public int Score;
+
+        public Node(byte[,] c, byte si, byte sn, Node f, Edge e, int s)
+        {
+            Cells = (byte[,])c.Clone();
+            Selecting = si;
+            SelectNum = sn;
+            From = f;
+            Swaped = e;
+            Score = s;
+        }
+    }
+}
